80ecc0a1eb9371d9fdf2de3f9e2ec4ebd1bb3ba5
Nugroho Dewantorto <nugroho.dewantoro@gmail.com>
2022-11-18 03:03:25 +0700

    fix

--- utility/Utility.hpp
+++ utility/Utility.hpp
@@ -9,4 +9,4 @@ T RoundToNextMultiple(const T a, const T multiple)
     return ((a + multiple - 1) / multiple) * multiple;
 }
 
-std::vector<char> readFile(const std::string& filename);
+std::vector<char> readFile(const char* filename);
