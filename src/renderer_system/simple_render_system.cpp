02d4c936215587f399b68fad0a196650ec7f50eb
Nugroho Dewantorto <nugroho.dewantoro@gmail.com>
2022-11-20 07:12:14 +0700

    revert

--- src/renderer_system/simple_render_system.cpp
+++ src/renderer_system/simple_render_system.cpp
@@ -66,8 +66,8 @@ namespace nugiEngine {
 		auto projectionView = camera.getProjectionMatrix() * camera.getViewMatrix();
 
 		for (auto& obj : gameObjects) {
-			// obj.transform.rotation.y = glm::mod(obj.transform.rotation.y + 0.01f, glm::two_pi<float>());
-			// obj.transform.rotation.x = glm::mod(obj.transform.rotation.x + 0.005f, glm::two_pi<float>());
+			obj.transform.rotation.y = glm::mod(obj.transform.rotation.y + 0.01f, glm::two_pi<float>());
+			obj.transform.rotation.x = glm::mod(obj.transform.rotation.x + 0.005f, glm::two_pi<float>());
 
 			SimplePushConstantData pushConstant{};
 			pushConstant.color = obj.color;
