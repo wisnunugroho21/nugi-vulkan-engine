ee6c9e2563ad6520923d35e4321378fc8154f892
Nugroho Dewantorto <nugroho.dewantoro@gmail.com>
2022-11-26 13:10:02 +0700

    use fragment lighting

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -53,7 +53,7 @@ namespace nugiEngine {
 
 		auto globalSetLayout = 
 			EngineDescriptorSetLayout::Builder(this->device)
-				.addBinding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_VERTEX_BIT)
+				.addBinding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS)
 				.build();
 
 		std::vector<VkDescriptorSet> globalDescriptorSets(EngineSwapChain::MAX_FRAMES_IN_FLIGHT);
