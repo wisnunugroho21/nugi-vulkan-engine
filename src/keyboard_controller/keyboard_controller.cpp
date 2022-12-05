cdfb1cbf6ae27e6ea621bd7bebefab6f5b2586e6
Nugroho Dewantorto <nugroho.dewantoro@gmail.com>
2022-12-05 07:46:32 +0700

    delete camera rotation from keyboard

--- src/keyboard_controller/keyboard_controller.cpp
+++ src/keyboard_controller/keyboard_controller.cpp
@@ -3,20 +3,6 @@
 namespace nugiEngine {
 
   void EngineKeyboardController::moveInPlaceXZ(GLFWwindow* window, float dt, EngineGameObject& gameObject) {
-    glm::vec3 rotate{0};
-
-    if (glfwGetKey(window, keymaps.lookRight) == GLFW_PRESS) rotate.y += 1.0f;
-    if (glfwGetKey(window, keymaps.lookLeft) == GLFW_PRESS) rotate.y -= 1.0f;
-    if (glfwGetKey(window, keymaps.lookUp) == GLFW_PRESS) rotate.x += 1.0f;
-    if (glfwGetKey(window, keymaps.lookDown) == GLFW_PRESS) rotate.x -= 1.0f;
-
-    if (glm::dot(rotate, rotate) > std::numeric_limits<float>::epsilon()) {
-      gameObject.transform.rotation += lookSpeed * dt * glm::normalize(rotate);
-    }
-
-    gameObject.transform.rotation.x = glm::clamp(gameObject.transform.rotation.x, -1.5f, 1.5f);
-    gameObject.transform.rotation.y = glm::mod(gameObject.transform.rotation.y, glm::two_pi<float>());
-
     float yaw = gameObject.transform.rotation.y;
     const glm::vec3 forwardDir{sin(yaw), 0.0f, cos(yaw)};
     const glm::vec3 rightDir{forwardDir.z, 0.0f, -forwardDir.x};
