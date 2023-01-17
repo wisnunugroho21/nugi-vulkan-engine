6161da1d0ffec851fba7d634d239be0c3d572033
Nugroho Dewantorto <nugroho.dewantoro@gmail.com>
2023-01-17 17:59:19 +0700

    fix

--- src/swap_chain/swap_chain.cpp
+++ src/swap_chain/swap_chain.cpp
@@ -93,7 +93,7 @@ namespace nugiEngine {
     createInfo.imageColorSpace = surfaceFormat.colorSpace;
     createInfo.imageExtent = extent;
     createInfo.imageArrayLayers = 1;
-    createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+    createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_STORAGE_BIT;
 
     QueueFamilyIndices indices = this->device.findPhysicalQueueFamilies();
     uint32_t queueFamilyIndices[] = {indices.graphicsFamily, indices.presentFamily};
