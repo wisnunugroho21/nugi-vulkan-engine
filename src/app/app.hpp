a780ff659d81cad9de221200e97acda0f9dc519d
Nugroho Dewantorto <nugroho.dewantoro@gmail.com>
2023-01-19 02:21:11 +0700

    change size

--- src/app/app.hpp
+++ src/app/app.hpp
@@ -16,8 +16,8 @@ namespace nugiEngine {
 	class EngineApp
 	{
 		public:
-			static constexpr int WIDTH = 1080;
-			static constexpr int HEIGHT = 1080;
+			static constexpr int WIDTH = 800;
+			static constexpr int HEIGHT = 800;
 
 			EngineApp();
 			~EngineApp();
