ebb8e3123e25ac0cf549f5f6461aa3d4bd27eef2
Nugroho Dewantoro <nugroho8dewantoro@gmail.com>
2023-01-19 07:44:11 +0700

    some fix

--- src/renderer_system/sampling_ray_render_system.cpp
+++ src/renderer_system/sampling_ray_render_system.cpp
@@ -43,7 +43,7 @@ namespace nugiEngine {
 		assert(this->pipelineLayout != nullptr && "Cannot create pipeline before pipeline layout");
 
 		this->pipeline = EngineComputePipeline::Builder(this->appDevice, this->pipelineLayout)
-			.setDefault("shader/ray_trace_weekend.comp.spv")
+			.setDefault("shader/ray_trace_sampling.comp.spv")
 			.build();
 	}
 
